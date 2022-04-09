a18018319399fe7747e34fc7e5fc5ad74d8f165c
Alexander Sribnyak <alexandersribnyak@gmail.com>
2022-04-09 13:47:30 +0300

    add rm, rmdir, mkdir, hw, cat; little refactoring of main section

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ class Command:
             print(f'{PROJECT_NAME}: {self.name}: an error occurred: {error}')
 
 
+def hello(filename: str):
+    with open(filename, 'w') as file:
+        print("Hello, world!", file=file)
+
+
+def cat(filename: str):
+    with open(filename, 'r') as file:
+        print(file.read(), end='')
+
+
 def my_help():
     print('Available commands:')
     for command in commands:
@@ -37,9 +47,14 @@ commands = [
     Command('ls', 0,
             lambda: print(*sorted(os.listdir()), sep='  '),
             'list directory contents'),
-    Command('cd', 1,
-            os.chdir,
-            'change the current working directory'),
+    Command('cd', 1, os.chdir, 'change the current working directory'),
+    # todo cp
+    # todo mv
+    Command('rm', 1, os.remove, 'remove a file'),
+    Command('rmdir', 1, os.rmdir, 'remove an empty directory'),
+    Command('mkdir', 1, os.mkdir, 'make a directory'),
+    Command('hw', 1, hello, 'make a text file with "Hello, world!" in it'),
+    Command('cat', 1, cat, 'print file contents'),
     Command('help', 0, my_help, 'print this help message'),
     Command('exit', 0, lambda: None, 'cause normal process termination'),
 ]
@@ -47,20 +62,26 @@ commands = [
 commands_dict = {command.name: command for command in commands}
 
 
-if __name__ == '__main__':
+def run_command(command: str):
+    if not command:
+        return
+    command, *args = command.split()
+    if command in commands_dict:
+        commands_dict[command].run(*args)
+    else:
+        print(f'{PROJECT_NAME}: {command}: command not found')
+
+
+def main():
     print(f'Welcome to {PROJECT_NAME} by {AUTHOR}')
     print('Type "help" for more information.')
 
     while True:
-        command = input('$ ').split()
-        if not command:
-            continue
-
-        command, *args = command
-        if command not in commands_dict:
-            print(f'{PROJECT_NAME}: {command}: command not found')
-            continue
-        commands_dict[command].run(*args)
-
-        if command == 'exit' and len(args) == 0:
+        command = input('$ ').strip()
+        run_command(command)
+        if command == 'exit':
             break
+
+
+if __name__ == '__main__':
+    main()
